bedbb1973c3dc96a9a6d2b09fddbea40050d832d
AlxndrVgt <alexander.vogt@avolutions.de>
2025-08-21 22:04:20 +0200

    feat: add country domain

---
+++ src/Countries/CountriesModule.cs
@@ -0,0 +1,16 @@
+﻿using Avolutions.Baf.Core.Entity.Abstractions;
+using Avolutions.Baf.Core.Module.Abstractions;
+using Avolutions.Baf.Domain.Countries.Models;
+using Avolutions.Baf.Domain.Countries.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Avolutions.Baf.Domain.Countries;
+
+public class CountriesModule : IFeatureModule
+{
+    public void Register(IServiceCollection services)
+    {
+        services.AddScoped<CountryService>();
+        services.AddScoped<ITranslatableEntityService<Country, CountryTranslation>, CountryService>();
+    }
+}
---
+++ src/Countries/Services/CountryService.cs
@@ -0,0 +1,17 @@
+﻿using Avolutions.Baf.Core.Entity.Services;
+using Avolutions.Baf.Domain.Countries.Models;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Avolutions.Baf.Domain.Countries.Services;
+
+public class CountryService : TranslatableEntityService<Country, CountryTranslation>
+{
+    public CountryService(DbContext context) : base(context)
+    {
+    }
+
+    public CountryService(DbContext context, IValidator<Country>? validator) : base(context, validator)
+    {
+    }
+}
