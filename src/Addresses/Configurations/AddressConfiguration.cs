3dbf6ec1f10428c92c983af0c447f867263e908f
AlxndrVgt <alexander.vogt@avolutions.de>
2025-09-02 20:28:47 +0200

    feat: add addresses

---
+++ src/Addresses/Configurations/AddressConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Avolutions.Baf.Domain.Addresses.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Avolutions.Baf.Domain.Addresses.Configurations;
+
+public class AddressConfiguration : IEntityTypeConfiguration<Address>
+{
+    public void Configure(EntityTypeBuilder<Address> builder)
+    {
+        builder.Navigation(a => a.Country)
+            .AutoInclude();
+    }
+}
---
+++ src/Addresses/Models/Address.cs
@@ -0,0 +1,26 @@
+﻿using Avolutions.Baf.Core.Entity.Models;
+using Avolutions.Baf.Domain.Countries.Models;
+
+namespace Avolutions.Baf.Domain.Addresses.Models;
+
+public class Address : EntityBase
+{
+    public string? AdditionalLine { get; set; }
+    public string? Street { get; set; }
+    public string? ZipCode { get; set; }
+    public string City { get; set; } = string.Empty;
+    public Guid CountryId { get; set; }
+    public Country Country { get; set; } = null!;
+    
+    public override string GetName()
+    {
+        var street = Street?.Trim();
+        var location = $"{ZipCode} {City}".Trim();
+
+        return string.IsNullOrWhiteSpace(street)
+            ? location
+            : string.IsNullOrWhiteSpace(location)
+                ? street
+                : $"{street}, {location}";
+    }
+}
