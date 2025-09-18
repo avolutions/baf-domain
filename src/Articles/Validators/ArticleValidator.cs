44544682f350cdc249fa6028b8a8b54f169ea835
AlxndrVgt <alexander.vogt@avolutions.de>
2025-09-18 13:01:27 +0200

    feat: use CreateOrUpdate ruleset in article validator

--- src/Articles/Validators/ArticleValidator.cs
+++ src/Articles/Validators/ArticleValidator.cs
@@ -1,4 +1,5 @@
 ﻿using Avolutions.Baf.Core;
+using Avolutions.Baf.Core.Validation.Abstractions;
 using Avolutions.Baf.Core.Validation.Extensions;
 using Avolutions.Baf.Domain.Articles.Models;
 using Avolutions.Baf.Domain.Articles.Resources;
@@ -12,9 +13,8 @@ public class ArticleValidator : AbstractValidator<Article>
     public ArticleValidator(BafDbContext dbContext, IStringLocalizer<ArticleResources> localizer)
     {
         RuleFor(x => x.ArticleNo)
-            .NotEmpty()
-            .WithName(localizer["Field.ArticleNo"])
             .UniqueFor(dbContext, x => x.Id)
+            .WithName(localizer["Field.ArticleNo"])
             .When(x => !string.IsNullOrWhiteSpace(x.ArticleNo));
 
         RuleFor(x => x.Title)
@@ -30,5 +30,12 @@ public class ArticleValidator : AbstractValidator<Article>
             .WithName(localizer["Field.TaxRate"])
             .InclusiveBetween(0, 100)
             .WithMessage(localizer["Validation.TaxRateRange"]);
+        
+        RuleSet(RuleSets.CreateOrUpdate, () => 
+        {
+            RuleFor(x => x.ArticleNo)
+                .NotEmpty()
+                .WithName(localizer["Field.ArticleNo"]);
+        });
     }
 }
