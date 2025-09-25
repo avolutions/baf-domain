ee8b71d792cb872d686b91594847fbda2262c025
AlxndrVgt <alexander.vogt@avolutions.de>
2025-09-25 16:34:32 +0200

    feat: add person interface

---
+++ src/Persons/Abstractions/IPerson.cs
@@ -0,0 +1,16 @@
+﻿using Avolutions.Baf.Domain.AcademicTitles.Models;
+using Avolutions.Baf.Domain.Salutations.Models;
+
+namespace Avolutions.Baf.Domain.Persons.Abstractions;
+
+public interface IPerson
+{
+    public string? FirstName { get; set; }
+    public string LastName { get; set; }
+    
+    public Guid? SalutationId { get; set; }
+    public Salutation? Salutation { get; set; }
+    
+    public Guid? AcademicTitleId { get; set; }
+    public AcademicTitle? AcademicTitle { get; set; }
+}
---
+++ src/Persons/Extensions/PersonExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Avolutions.Baf.Domain.Persons.Abstractions;
+
+namespace Avolutions.Baf.Domain.Persons.Extensions;
+
+public static class PersonExtensions
+{
+    public static string FullName(this IPerson person)
+    {
+        return $"{person.AcademicTitle?.Value} {person.FirstName} {person.LastName}".Trim();
+    }
+}
