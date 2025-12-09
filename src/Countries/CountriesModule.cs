4153269c82fc77fb354d71ea8012214af79eb8e6
AlxndrVgt <alexander.vogt@avolutions.de>
2025-12-09 17:17:20 +0100

    refactor: countries to new lookups

--- src/Countries/CountriesModule.cs
+++ src/Countries/CountriesModule.cs
@@ -1,7 +1,6 @@
-﻿using Avolutions.Baf.Core.Entity.Abstractions;
+﻿using Avolutions.Baf.Core.Lookups.Extensions;
 using Avolutions.Baf.Core.Module.Abstractions;
 using Avolutions.Baf.Domain.Countries.Models;
-using Avolutions.Baf.Domain.Countries.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Avolutions.Baf.Domain.Countries;
@@ -10,8 +9,6 @@ public class CountriesModule : IFeatureModule
 {
     public void Register(IServiceCollection services)
     {
-        services.AddScoped<CountryService>();
-        services.AddScoped<IEntityService<Country>, CountryService>();
-        services.AddScoped<ITranslatableEntityService<Country, CountryTranslation>, CountryService>();
+        services.AddLookup<Country>();
     }
 }
