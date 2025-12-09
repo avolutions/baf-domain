34a2dfc8e10e28b3f274c5d7802ff752502a7284
AlxndrVgt <alexander.vogt@avolutions.de>
2025-12-09 17:17:36 +0100

    refactor: salutations to new lookups

---
+++ src/Salutations/SalutationModule.cs
@@ -0,0 +1,14 @@
+﻿using Avolutions.Baf.Core.Lookups.Extensions;
+using Avolutions.Baf.Core.Module.Abstractions;
+using Avolutions.Baf.Domain.Salutations.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Avolutions.Baf.Domain.Salutations;
+
+public class SalutationModule : IFeatureModule
+{
+    public void Register(IServiceCollection services)
+    {
+        services.AddLookup<Salutation>();
+    }
+}
