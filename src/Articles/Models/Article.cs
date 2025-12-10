26d7658c01626cbc1d4e705891d0f9c2e52fe715
AlxndrVgt <alexander.vogt@avolutions.de>
2025-12-10 13:30:04 +0100

    feat: make article navigable

--- src/Articles/Models/Article.cs
+++ src/Articles/Models/Article.cs
@@ -1,12 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using Avolutions.Baf.Core.Entity.Abstractions;
+using Avolutions.Baf.Core.Entity.Attributes;
 using Avolutions.Baf.Core.Entity.Models;
 using Avolutions.Baf.Domain.QuantityUnits.Models;
 
 namespace Avolutions.Baf.Domain.Articles.Models;
 
-public class Article : EntityBase
+public class Article : EntityBase, INavigable
 {
     public ArticleType Type { get; set; } = ArticleType.Product;
+    [EntityNavigationKey]
     public string ArticleNo { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public string? Description { get; set; }
