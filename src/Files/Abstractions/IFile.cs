dc81ac44345691295c93cfe3a1e64387402da390
AlxndrVgt <alexander.vogt@avolutions.de>
2025-12-29 18:11:20 +0100

    feat: add content to file

--- src/Files/Abstractions/IFile.cs
+++ src/Files/Abstractions/IFile.cs
@@ -6,4 +6,5 @@ public interface IFile
     public string FileExtension { get; set; }
     public string ContentType { get; set; }
     public long Size { get; set; }
+    public byte[] Content { get; set; }
 }
